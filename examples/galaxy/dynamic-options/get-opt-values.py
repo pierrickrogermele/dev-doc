f3bd72877fdd7c1aa8e7558324850bed059283da
Pierrick Roger Mele <pierrick.roger@gmail.com>
2016-08-31 21:40:33 +0200

    Improve dynamic_options example for Galaxy, using a conditional tag. Test it and correct it under Galaxy.

--- examples/galaxy/dynamic-options/get-opt-values.py
+++ examples/galaxy/dynamic-options/get-opt-values.py
@@ -3,3 +3,9 @@ def get_opt_values(length):
     for i, j in enumerate(range(length)):
         cols.append( (str(j), str(j), i == 0) )
     return cols
+
+def get_opt_values_2(typ):
+    cols = []
+    for i, j in enumerate(range(3)):
+        cols.append( (typ + str(j), typ + str(j), i == 0) )
+    return cols
