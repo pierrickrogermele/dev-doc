5d49ebcf416c816a8a17ea1b67e732fdd7097498
Pierrick Roger <pk.roger@icloud.com>
2019-11-29 16:08:26 +0100

    Add regex example for PHP.

---
+++ examples/php/regex.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php
+<?php
+
+var_dump(preg_replace('/^(immuno|zina).*$/i', "$1", "ZIMMUNO_aaaaaa"));
+var_dump(preg_replace('/^(immuno|zina).*$/i', "$1", "IMMUNO_aaaaaa"));
+var_dump(preg_replace('/^(immuno|zina).*$/i', "$1", "ImmUNO_aaaaaa"));
